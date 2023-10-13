21aabd60fdabe2aea5110557c00a441811d9338c
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-13 08:05:01 -0700

    changed some of the JS making it change text color when answer if wrong/right.

--- Number_Guessing_Game/Number Guessing Game.js
+++ Number_Guessing_Game/Number Guessing Game.js
@@ -1,11 +1,9 @@
-// import random class
-
+//declareing the value's of buttons
 const inputElement = document.getElementById("input");
 const button = document.getElementById("button")
-
 let answer = document.getElementById("guessANumber");
 
-
+//getting a random number between 1 and ten by getting a number between 0-1, multiplying by 10, rounding down, and adding 1
 var randomNum = Math.floor((Math.random() * 10) + 1)
 console.log(randomNum);
 
@@ -13,20 +11,24 @@ console.log(randomNum);
 function checkGuess() {
     let guess = inputElement.value;
 
-// if the number=guess it will be right
+    // if the number=guess it will turn green and say you're right
     if (randomNum == guess)
     {
         answer.innerText = "You're right, good job!"
-        content.style.color = "red"
+        answer.style.color = "green"
     } 
+    // if the players guess is greater than the random number then it will say your number is too high and turn red
     else if (guess > randomNum)
     {
         answer.innerText = "Your number is too high"
+        answer.style.color = "red"
     }
+    // if the players guess is less than the random number then it will say your number is too low and turn red
     else
     {
         answer.innerText = "Your number is too low"
+        answer.style.color = "red"
     }
 }
-
+// 
 button.addEventListener("click", checkGuess)
--- Number_Guessing_Game/Number_Guesing_game/README.md
+++
@@ -1,2 +0,0 @@
-# Number_Guesing_game
-making a number guessing game with html, css, and java
